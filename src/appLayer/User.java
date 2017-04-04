2f51fd275eb64a24d077606dab3757c71d864d18
Yoithebrain <crazyone5477@gmail.com>
2017-04-04 11:16:12 +0200

    Made a database and used it to fetch userlogin details instead of fetching it from hardcoded credentials.

--- src/appLayer/User.java
+++ src/appLayer/User.java
@@ -1,14 +1,14 @@
 package appLayer;
-
+import dataLayer.*;
 /**
  * Created by jakob on 21-03-2017.
  */
 public class User {
 
     public boolean isValidUserCredentials(String sUsername, String sUserPassword){
-        if (sUsername.equals("yoi") && sUserPassword.equals("toda")) {
-            return true;
-        }
-        return false;
+        userDB userDB = new userDB();
+
+        return userDB.isValidUserLogin(sUsername, sUserPassword);
     }
+
 }
